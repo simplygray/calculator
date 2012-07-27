be3b6f4e5b12ed442b87b8fe2d35aa1e71a439bd
Gray Zhi <gray@asap-inc.net>
2012-07-27 16:21:28 +0800

    add README

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -8,27 +8,10 @@
 
 #import "CalculatorViewController.h"
 
-@interface CalculatorViewController ()
-
-@end
-
 @implementation CalculatorViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+@synthesize display = _display;
+- (IBAction)digiPressed:(id)sender {
 }
 
 @end
